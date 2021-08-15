0f2683271e9e970d1a85d5bba5cb121ffd5e5b8d
Justinas Stankevicius <justinas@justinas.org>
2021-08-15 21:35:55 +0300

    Pin nixpkgs

---
+++ nixpkgs.nix
@@ -0,0 +1,6 @@
+builtins.fetchGit {
+  name = "nixos-21.05-2021-08-05";
+  url = "https://github.com/NixOS/nixpkgs";
+  ref = "refs/heads/nixos-21.05";
+  rev = "733682c32929293341f113f297b64ea6319e9089";
+}
--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import (import ./nixpkgs.nix) { } }:
 let
   makeImage = pkgs.writeShellScriptBin "make-image" ''
     nix-build image.nix -o image
